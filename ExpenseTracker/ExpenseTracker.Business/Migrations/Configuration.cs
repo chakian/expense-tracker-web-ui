a00448d7a03789b658658d6c583a93e6532fcfe4
Çağdaş Korkut <cagdaskorkut@gmail.com>
2019-05-07 21:32:51 +0300

    Created Initial Migration

---
+++ ExpenseTracker/ExpenseTracker.Business/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace ExpenseTracker.Business.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ExpenseTracker.Business.Context.ExpenseTrackerContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "ExpenseTracker.Business.Context.ExpenseTrackerContext";
+        }
+
+        protected override void Seed(ExpenseTracker.Business.Context.ExpenseTrackerContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
