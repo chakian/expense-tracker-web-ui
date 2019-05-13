b8a5e6c7467e7bfdb4043f4da767b3b12e63f570
Çağdaş Korkut <cagdaskorkut@gmail.com>
2019-05-13 22:14:25 +0300

    Update BaseModel.cs

--- ExpenseTracker/ExpenseTracker.WebUI/Models/Base/BaseModel.cs
+++ ExpenseTracker/ExpenseTracker.WebUI/Models/Base/BaseModel.cs
@@ -4,14 +4,6 @@ namespace ExpenseTracker.WebUI.Models
 {
     public class BaseModel
     {
-        //TODO: Delete This
-        public BaseModel()
-        {
-            User = new UserEntity()
-            {
-                ID = 1
-            };
-        }
         public bool IsLoggedIn => (User != null && User.ID > 0);
 
         public UserEntity User { get; set; }
