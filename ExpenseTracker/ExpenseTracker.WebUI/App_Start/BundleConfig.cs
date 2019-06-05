84ca3c0bf6262b7c4aff7ebfed1c90e14574a5dd
Çağdaş Korkut <cagdaskorkut@gmail.com>
2019-06-05 11:29:46 +0300

    Deleted old theme. Added Product Admin theme.

--- ExpenseTracker/ExpenseTracker.WebUI/App_Start/BundleConfig.cs
+++ ExpenseTracker/ExpenseTracker.WebUI/App_Start/BundleConfig.cs
@@ -26,42 +26,25 @@ namespace ExpenseTracker.WebUI
                       "~/Scripts/bootstrap.js"
                       ));
 
+            bundles.Add(new StyleBundle("~/Content/bootstrapcss").Include(
+                      "~/Content/bootstrap.css"
+                      ));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
                       "~/Content/site.css"
                       ));
 
-            bundles.Add(new StyleBundle("~/Content/theme/chimpcss").Include(
-                      "~/Content/theme/chimp/css/aos.css",
-                      "~/Content/theme/chimp/css/bootstrap.min.css",
-                      "~/Content/theme/chimp/css/bootstrap-datepicker.css",
-                      "~/Content/theme/chimp/css/jquery-ui.css",
-                      "~/Content/theme/chimp/css/magnific-popup.css",
-                      "~/Content/theme/chimp/css/mediaelementplayer.css",
-                      "~/Content/theme/chimp/css/owl.carousel.min.css",
-                      "~/Content/theme/chimp/css/owl.theme.default.min.css",
-                      "~/Content/theme/chimp/css/style.css",
-                      "~/Content/theme/chimp/css/bootstrap/bootstrap.css",
-                      "~/Content/theme/chimp/css/bootstrap/bootstrap-grid.css",
-                      "~/Content/theme/chimp/css/bootstrap/bootstrap-reboot.css"
+            bundles.Add(new StyleBundle("~/Content/theme/prodadmcss").Include(
+                      "~/Content/theme/prod_adm/css/bootstrap.min.css",
+                      "~/Content/theme/prod_adm/css/fontawesome.min.css",
+                      "~/Content/theme/prod_adm/css/templatemo-style.css"
                       ));
 
-            bundles.Add(new ScriptBundle("~/bundles/theme/chimpjs").Include(
-                      "~/Content/theme/chimp/js/aos.js",
+            bundles.Add(new ScriptBundle("~/bundles/theme/prodadmjs").Include(
                       "~/Content/theme/chimp/js/bootstrap.min.js",
-                      "~/Content/theme/chimp/js/bootstrap-datepicker.min.js",
-                      "~/Content/theme/chimp/js/jquery.countdown.min.js",
-                      "~/Content/theme/chimp/js/jquery.magnific-popup.min.js",
-                      "~/Content/theme/chimp/js/jquery.stellar.min.js",
-                      "~/Content/theme/chimp/js/jquery-3.3.1.min.js",
-                      "~/Content/theme/chimp/js/jquery-migrate-3.0.1.min.js",
-                      "~/Content/theme/chimp/js/jquery-ui.js",
-                      "~/Content/theme/chimp/js/main.js",
-                      "~/Content/theme/chimp/js/mediaelement-and-player.min.js",
-                      "~/Content/theme/chimp/js/owl.carousel.min.js",
-                      "~/Content/theme/chimp/js/popper.min.js",
-                      "~/Content/theme/chimp/js/slick.min.js",
-                      "~/Content/theme/chimp/js/typed.js"
+                      "~/Content/theme/chimp/js/Chart.min.js",
+                      "~/Content/theme/chimp/js/moment.min.js",
+                      "~/Content/theme/chimp/js/tooplate-scripts.js"
                       ));
         }
     }
