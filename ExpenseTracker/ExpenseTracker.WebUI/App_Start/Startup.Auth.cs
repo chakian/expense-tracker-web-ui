5e6e0195391632c8397ff82685cd6a6d0c43ac73
Çağdaş Korkut <cagdaskorkut@gmail.com>
2019-06-11 22:40:45 +0300

    Extended cookie time for authenticated identity period close #56

--- ExpenseTracker/ExpenseTracker.WebUI/App_Start/Startup.Auth.cs
+++ ExpenseTracker/ExpenseTracker.WebUI/App_Start/Startup.Auth.cs
@@ -30,7 +30,7 @@ namespace ExpenseTracker.WebUI
                     // Enables the application to validate the security stamp when the user logs in.
                     // This is a security feature which is used when you change a password or add an external login to your account.  
                     OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ExpenseUserManager, User>(
-                        validateInterval: TimeSpan.FromMinutes(30),
+                        validateInterval: TimeSpan.FromDays(10),
                         regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
                 }
             });
