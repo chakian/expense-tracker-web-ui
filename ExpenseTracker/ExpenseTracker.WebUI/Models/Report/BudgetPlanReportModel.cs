f708fb4aeffcc7ec92f2265179d375ce42bd8abf
Çağdaş Korkut <cagdaskorkut@gmail.com>
2019-06-29 13:38:48 +0300

    Added a report page. close #34
    It is not the most awesome report page I have created. But it'll have to do until I prepare a new theme for the whole front-end.

---
+++ ExpenseTracker/ExpenseTracker.WebUI/Models/Report/BudgetPlanReportModel.cs
@@ -0,0 +1,12 @@
+﻿using ExpenseTracker.WebUI.Models.ContextObjects;
+using System.Collections.Generic;
+
+namespace ExpenseTracker.WebUI.Models.Report
+{
+    public class BudgetPlanReportModel : BaseModel
+    {
+        public BudgetPlan BudgetPlan { get; set; }
+
+        public List<BudgetPlanCategory> PlanCategories { get; set; }
+    }
+}
