4108b68b76d0836cb6f275131a1e472924603cdc
Çağdaş Korkut <cagdaskorkut@gmail.com>
2019-07-09 00:25:02 +0300

    Transaction Drafts. see #32

---
+++ ExpenseTracker/ExpenseTracker.Business.Tests/TransactionDraftTests/CreateTransactionDraftTests.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+
+namespace ExpenseTracker.Business.Tests.TransactionDraftTests
+{
+    public class CreateTransactionDraftTests
+    {
+        [Theory]
+        [InlineData(null, null, null, 1)]
+        [InlineData(null, null, 2, null)]
+        [InlineData(null, null, 2, 1)]
+        [InlineData(null, "sigara", null, null)]
+        [InlineData(null, "sigara", null, 1)]
+        [InlineData(null, "sigara", 2, null)]
+        [InlineData(null, "sigara", 2, 1)]
+        [InlineData(15, null, null, null)]
+        [InlineData(15, null, null, 1)]
+        [InlineData(15, null, 2, null)]
+        [InlineData(15, null, 2, 1)]
+        [InlineData(15, "sigara", null, null)]
+        [InlineData(15, "sigara", null, 1)]
+        [InlineData(15, "sigara", 2, null)]
+        [InlineData(15, "sigara", 2, 1)]
+        public void CreateTransactionDraft_Success(decimal? amount, string description, int? categoryId, int? sourceAccountId)
+        {
+        }
+
+        [Fact]
+        public void CreateTransactionDraft_Fail()
+        {
+        }
+    }
+}
---
+++ ExpenseTracker/ExpenseTracker.Business.Tests/TransactionDraftTests/EditTransactionDraftTests.cs
@@ -0,0 +1,13 @@
+﻿//TODO: Not implemented exception ()
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ExpenseTracker.Business.Tests.TransactionDraftTests
+//{
+//    class EditTransactionDraftTests
+//    {
+//    }
+//}
