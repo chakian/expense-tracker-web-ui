f545558beed8cbbaf86addde40827ce5e627b1c6
Çağdaş Korkut <cagdaskorkut@gmail.com>
2019-09-24 21:09:04 +0300

    Another TDD approach bites the dust. JK I'll write them from scratch.

--- ExpenseTracker/ExpenseTracker.Business.Tests/TransactionTests/GetTransactionListTests.cs
+++ ExpenseTracker/ExpenseTracker.Business.Tests/TransactionTests/GetTransactionListTests.cs
@@ -1,27 +1,27 @@
-﻿using ExpenseTracker.Business.Tests.Base;
-using ExpenseTracker.Entities;
-using System;
-using System.Collections.Generic;
-using Xunit;
+﻿//using ExpenseTracker.Business.Tests.Base;
+//using ExpenseTracker.Entities;
+//using System;
+//using System.Collections.Generic;
+//using Xunit;
 
-namespace ExpenseTracker.Business.Tests.TransactionTests
-{
-    public class GetTransactionListTests : BaseTransactionTest
-    {
-        [Fact]
-        public void GetTransactionsForPeriod_Success()
-        {
-            // ARRANGE
-            TransactionBusiness transactionBusiness = new TransactionBusiness(context);
-            int periodYear = 2019, periodMonth = 9;
-            DateTime periodDate = new DateTime(periodYear, periodMonth, 23);
+//namespace ExpenseTracker.Business.Tests.TransactionTests
+//{
+//    public class GetTransactionListTests : BaseTransactionTest
+//    {
+//        [Fact]
+//        public void GetTransactionsForPeriod_Success()
+//        {
+//            // ARRANGE
+//            TransactionBusiness transactionBusiness = new TransactionBusiness(context);
+//            int periodYear = 2019, periodMonth = 9;
+//            DateTime periodDate = new DateTime(periodYear, periodMonth, 23);
 
-            // ACT
-            List<TransactionEntity> actual = transactionBusiness.GetTransactionsForPeriodByGivenDate(periodDate, DefaultUserId, DefaultTestBudgetId);
+//            // ACT
+//            List<TransactionEntity> actual = transactionBusiness.GetTransactionsForPeriodByGivenDate(periodDate, DefaultUserId, DefaultTestBudgetId);
 
-            // ASSERT
-            Assert.NotNull(actual);
-            Assert.NotEmpty(actual);
-        }
-    }
-}
+//            // ASSERT
+//            Assert.NotNull(actual);
+//            Assert.NotEmpty(actual);
+//        }
+//    }
+//}
--- ExpenseTracker/ExpenseTracker.Business.Tests/TransactionTests/NewTransactionTests.cs
+++ ExpenseTracker/ExpenseTracker.Business.Tests/TransactionTests/NewTransactionTests.cs
@@ -1,26 +1,26 @@
-﻿using ExpenseTracker.Business.Tests.Base;
-using ExpenseTracker.Entities;
-using Xunit;
+﻿//using ExpenseTracker.Business.Tests.Base;
+//using ExpenseTracker.Entities;
+//using Xunit;
 
-namespace ExpenseTracker.Business.Tests.TransactionTests
-{
-    public class NewTransactionTests : BaseTransactionTest
-    {
-        [Fact]
-        public void CreateFirstCategory_Expense_Success()
-        {
-            // ARRANGE
-            TransactionBusiness transactionBusiness = new TransactionBusiness(context);
+//namespace ExpenseTracker.Business.Tests.TransactionTests
+//{
+//    public class NewTransactionTests : BaseTransactionTest
+//    {
+//        [Fact]
+//        public void CreateFirstCategory_Expense_Success()
+//        {
+//            // ARRANGE
+//            TransactionBusiness transactionBusiness = new TransactionBusiness(context);
 
-            TransactionEntity transactionEntity = new TransactionEntity()
-            {
-            };
+//            TransactionEntity transactionEntity = new TransactionEntity()
+//            {
+//            };
 
-            // ACT
-            TransactionEntity actual = transactionBusiness.InsertTransaction(transactionEntity, DefaultUserId);
+//            // ACT
+//            TransactionEntity actual = transactionBusiness.InsertTransaction(transactionEntity, DefaultUserId);
 
-            // ASSERT
-            Assert.NotNull(actual);
-        }
-    }
-}
+//            // ASSERT
+//            Assert.NotNull(actual);
+//        }
+//    }
+//}
