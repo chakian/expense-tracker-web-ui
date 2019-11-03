6c267546b652ab2c2b386f85e75909da70bad22a
Çağdaş Korkut <cagdaskorkut@gmail.com>
2019-11-03 16:59:43 +0300

    Added Filtering options to TransactionList page. Filtering options are category and account

--- ExpenseTracker/ExpenseTracker.WebUI/Models/Base/BaseTransactionModel.cs
+++ ExpenseTracker/ExpenseTracker.WebUI/Models/Base/BaseTransactionModel.cs
@@ -15,8 +15,10 @@ namespace ExpenseTracker.WebUI.Models
             public int TransactionId { get; set; }
             public DateTime Date { get; set; }
             public decimal Amount { get; set; }
+            public int CategoryId { get; set; }
             public string Category { get; set; }
             public string Description { get; set; }
+            public int AccountId { get; set; }
             public string Account { get; set; }
         }
     }
