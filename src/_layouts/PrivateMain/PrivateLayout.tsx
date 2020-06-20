73e61bc236cd27cbdfcced32c58077169c43c650
Çağdaş Korkut <cagdaskorkut@gmail.com>
2020-06-20 15:18:00 +0300

    styled PrivateLayout with antd

--- src/_layouts/PrivateMain/PrivateLayout.tsx
+++ src/_layouts/PrivateMain/PrivateLayout.tsx
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component, CSSProperties } from 'react';
+
+import { Layout } from 'antd';
+const { Header, Footer, Content, Sider } = Layout;
 
 import TopBar from './TopBar';
+
+const textCenter: CSSProperties = {
+    textAlign: "center"
+};
 
 class PrivateLayout extends Component {
     render() { 
         return(
             <div>
-                <TopBar />
-                <div>Sidebar Navigation</div>
-                <main>
+                <Header style={textCenter}>
+                    <TopBar />
+                </Header>
+                <Content>
+                    <Sider>Sidebar Navigation</Sider>
                     {this.props.children}
-                    <div>
-                        Footer
-                        <br/><br/><br/><hr/><br/><br/>
-                        <div>
-                            <p>
-                                <a href="https://chakian.com" target="_blank">Chakian</a>
-                            </p>
-                            <p>
-                                <a href="https://cagdaskorkut.com" target="_blank">Çağdaş Korkut</a>
-                            </p>
-                        </div>
-                    </div>
-                </main>
+                </Content>
+                <Footer>
+                    by Çağdaş Korkut
+                </Footer>
             </div>
         );
     }
