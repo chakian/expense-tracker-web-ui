378a063eda5a6eaf3fea4fa63595d82830868b78
Çağdaş Korkut <cagdaskorkut@gmail.com>
2020-07-11 22:58:38 +0300

    change budget page with dummy data

--- src/budget/SwitchBudget.tsx
+++ src/budget/SwitchBudget.tsx
@@ -1,8 +1,51 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Dispatch, AnyAction, bindActionCreators } from 'redux';
+import { Table, Space } from 'antd';
 
 import { AppState } from '../_store/rootReducer';
+
+const columns = [
+    {
+        title: 'Bütçe Adı',
+        dataIndex: 'budgetName',
+        key: 'budgetName',
+    },
+    {
+        title: 'Son Güncelleme',
+        dataIndex: 'lastUpdate',
+        key: 'lastUpdate',
+    },
+    {
+        title: 'Değişim',
+        key: 'action',
+        render: (text, record) =>
+            (
+                <Space size="middle">
+                    {
+                        record.isDefault == true ?
+                            "Aktif" :
+                            <a>Aktifleştir</a>
+                    }
+                </Space>
+            ),
+    },
+];
+
+const data = [
+    {
+        key: '1',
+        budgetName: 'Deneme',
+        lastUpdate: '2020-01-01',
+        isDefault: true,
+    },
+    {
+        key: '2',
+        budgetName: 'Deneme 2',
+        lastUpdate: '2020-07-01',
+        isDefault: false,
+    },
+];
 
 class SwitchBudget extends Component<ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps>, {}> {
     componentDidMount() {
@@ -12,6 +55,7 @@ class SwitchBudget extends Component<ReturnType<typeof mapStateToProps> & Return
         return (
             <div>
                 <header>Bütçe Değiştir</header>
+                <Table columns={columns} dataSource={data} />
             </div>
         );
     }
